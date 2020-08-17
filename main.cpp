b5e353f2e5597caee79b4729d8b5484e47a9cb97
Oscar Clemente <oscar.clemente@gmail.com>
2020-08-17 11:30:12 +0200

    Added fuzzy metal.

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main()
 {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1920;
     const int image_height = static_cast<int>(image_width/aspect_ratio);
     const int samples_per_pixel = 20;
     const int max_depth = 30;
@@ -40,8 +40,8 @@ int main()
     
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.7);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0, 0, -1.0), 0.5, material_center));
